ffaef7d049b1ed0793f9b3a8abb5372090e43157
Administrator <admin@example.com>
2024-03-27 15:59:25 +0800

    setup 语法糖

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -7,49 +7,44 @@
         <button @click="changeAge">修改年龄</button>
         <button @click="showTel">查看联系方式</button>
     </div>
-    </template>
-    
-    <script lang="ts">
-import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript'
+</template>
 
-    //js 或 ts 内容 
-        export default{
-            name:'Person',
-            setup() {
-                let name = '张三' //注意，此时name 不是响应式
-                let age  = 16
-                let tel  = '13777777777'
+<script lang="ts">
+//js 或 ts 内容 
+export default {
+    name: 'Person',
+}
+</script>
 
-                //方法
-                function changeName()
-                {
-                    name ='李四'
-                    console.log(name)
-                }
-                function changeAge()
-                {
-                    age +=1
-                    console.log(age)
-                }
-                function showTel()
-                {
-                    alert(tel)
-                }
-                return {name,age,changeName,changeAge,showTel}
-            },
-        }
-    </script>
-    
-    <style scoped >
-    /*样式*/
-    .person{
-        background-color: skyblue ;
-        box-shadow: 0 0 10px;
-        border-radius: 10px;
-        padding: 20px;
+<script lang="ts" setup>
+    let name = '张小三' //注意，此时name 不是响应式
+    let age = 16
+    let tel = '13777777777'
+
+    //方法
+    function changeName() {
+        name = '李四'
+        console.log(name)
     }
-    button{
-        margin:  0 5px;
+    function changeAge() {
+        age += 1
+        console.log(age)
     }
-    
-    </style>
+    function showTel() {
+        alert(tel)
+    }
+</script>
+
+<style scoped>
+/*样式*/
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    margin: 0 5px;
+}
+</style>
