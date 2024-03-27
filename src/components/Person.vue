d6c0dd6226b5465ebbc2c90935057649818b9c75
Administrator <admin@example.com>
2024-03-27 21:45:06 +0800

    watch 监视数据发生变化

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,38 +1,71 @@
 <template>
     <div class="person">
-        姓:<input type="text" v-model ="firstName"> <br>  <!-- v-mode 双向绑定 value 数据到页面单向绑定-->
-        名:<input type="text" v-model ="lastName"> <br>
-        全名:<span> {{fullName}} </span><br>
-        <button @click="changeFullName">修改全名</button>
+        <h2>
+            求和：{{ sum }}
+        </h2>
+        <button @click="changeSum"> 点我累加 </button>
+        <h3>
+            姓名:{{ person.name }}  年龄:{{ person.age }}
+        </h3>
+        <button @click="changeAge"> 点我增加年龄 </button>
+
+        <h3>
+            品牌:{{ car.brand }}  价格:{{ car.price }}
+        </h3>
+        <button @click="changeBrand"> 点我修改品牌 </button>
+        <button @click="changePrice"> 点我修改价格 </button>
     </div>
 
 </template>
 <script lang="ts" setup name="Person">
-    import { ref,computed } from "vue"; //对象类型的响应数据
-    let firstName =ref( "zhang")
-    let lastName =ref ("san")
-
-    //这么定义的computed属性是只可读，不可修改
-    // let fullName =computed( () =>{
-    //     return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + 
-    //     lastName.value.slice(0,1).toUpperCase()+lastName.value.slice(1)
-    // })
-    //这么定义computed属性可修改
-    let fullName =computed({
-        get(){
-            return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + 
-            lastName.value.slice(0,1).toUpperCase()+lastName.value.slice(1)
-        },
-        set(val){
-            const [s1,s2] = val.split('-')
-            firstName.value=s1
-            lastName.value=s2
-        }
-    })
-    function changeFullName(){
-        fullName.value ='Li-Si'
-    }
+import { ref ,watch} from "vue"; 
+    let sum =ref( 0 )
     
+    
+    function changeSum(){
+        sum.value+=1
+    }
+
+    //监视ref/reactive 声明的响应式基本类型变量
+    watch (sum, (newValue,oldValue) =>{
+        console.log(newValue,oldValue)
+    })
+
+    let person = ref({name:'张三',age:18})
+    function changeAge(){
+        person.value.age +=1
+    }
+
+    //监视ref/reactive 声明的响应式对象类型变量，启用deep配置项监视对象的字段, 
+    //监视reactive默认的是深度监视,且不可关闭（deep:false 失效)
+    watch(person,(newValue,oldValue)=>{
+        console.log(newValue,oldValue);
+        if (newValue.age>=30)
+            person.value.age = 30
+
+    },{deep:true})
+
+    let car =ref({brand:'奔驰',price:100,yh:'8L/100km' ,mirror:{left:'left',right:'right'} })
+    function changeBrand(){
+        car.value.brand+='~'
+    }
+    function changePrice(){
+        car.value.price+=1
+    }
+    //监视一个函数的返回值，可以实现监听某个对象的字段，且字段是基本类型
+    watch( ()=>{return car.value.price}, (newValue,oldValue) =>{
+        console.log(newValue,oldValue)
+    })
+    //字段是对象，可以直接监视，也可以写成函数，建议写成函数（这样直接对car赋值，mirror也能被监视）
+    watch(car.value.mirror, (newValue,oldValue) =>{
+        console.log(newValue,oldValue)
+    })
+
+    //监视数组
+    watch([()=> car.value.brand,()=>car.value.yh],(newValue,oldValue) =>{
+        console.log(newValue,oldValue)
+    })
+
 </script>
 
 <style scoped>
