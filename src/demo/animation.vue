b5c6009ad54aa58b463e9d1c91105964fdc88c25
Administrator <admin@example.com>
2024-04-09 09:41:32 +0800

    第三方库animate.css 实现动画

--- src/demo/animation.vue
+++ src/demo/animation.vue
@@ -3,13 +3,18 @@
         <h2   > animation </h2>
         <input type ="button" value ="切换" @click="flag = !flag " />
         <Transition>
-        <h2 v-if ="flag" > 示例 </h2>
+        <h2 v-if ="flag" > 代码示例 </h2>
+        </Transition>
+
+        <Transition enter-active-class="animate__animated animate__bounceIn" leave-active-class="animate__animated animate__bounceOut">
+        <h2 v-if ="flag"   > animate.css 示例 </h2>
         </Transition>
         
     </div>
 </template>
 <script lang="ts" setup name="functions">
     import{ref} from 'vue'
+    import  'animate.css'
     let flag = ref(true)
     
     
